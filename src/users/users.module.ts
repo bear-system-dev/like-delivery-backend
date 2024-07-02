4ac474cd4e9c0bb464d4c21d5d1d46f2a5e2cd0d
LestterX <lgs.alves@hotmail.com>
2024-07-02 19:50:53 -0300

    SOLVED: import database module to users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { DatabaseMdodule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseMdodule],
   controllers: [UsersController],
   providers: [UsersService],
 })
